a779a5ecd48fc72c7f5bc1b5da7d2be8a348c21d
Oren D. Eisner <oren@theeisners.net>
2023-06-22 17:13:47 -0400

    Junk

--- ddb.js
+++ ddb.js
@@ -1,9 +1,7 @@
 //const { DynamoDB, ListTablesCommand } = require("aws-sdk/client-dynamodb");
 import pkg from 'aws-sdk';
 const {AWS, DynamoDB} = pkg;
-AWS.config.update({region:'us-east-1',
-aws_access_key_id : "AKIA4LVOLGWQY2UJDU42",
-    aws_secret_access_key : "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" });
+AWS.config.update({region:'us-east-1' });
 
 (async () => {
   const client = new DynamoDB();
